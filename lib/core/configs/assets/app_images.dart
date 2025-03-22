c78d0f5b1342615e112bf70d8a36fcd9541564c1
Lutfino Athallah Pratama <finolut@gmail.com>
2025-03-23 00:28:43 +0700

    update ke 2

--- lib/core/configs/assets/app_images.dart
+++ lib/core/configs/assets/app_images.dart
@@ -3,4 +3,5 @@ class AppImages {
 
   static const String introBG = '${basePath}intro_bg.jpg';
   static const String chooseModeBG = '${basePath}choose_mode_bg.jpg';
+  static const String authBG = '${basePath}auth_bg.png';
 }
--- lib/presentation/choose_mode/pages/choose_mode.dart
+++ lib/presentation/choose_mode/pages/choose_mode.dart
@@ -1,8 +1,14 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:musik/common/widgets/button/basic_app_button.dart';
 import 'package:musik/core/configs/assets/app_images.dart';
 import 'package:musik/core/configs/assets/app_vectors.dart';
+import 'package:musik/core/configs/theme/app_colors.dart';
+import 'package:musik/presentation/auth/pages/signup_or_signin.dart';
+import 'package:musik/presentation/choose_mode/bloc/theme_cubit.dart';
 
 
 class ChooseModePage extends StatelessWidget {
@@ -51,15 +57,93 @@ Padding(
           fontSize: 18,
         ),
       ),
-      SizedBox(height: 50),
- 
-  const SizedBox(height: 20),
+      SizedBox(height: 40),
+
+Row(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: [
+Column(
+  children: [
+     GestureDetector(
+                          onTap: (){
+                            context.read<ThemeCubit>().updateTheme(ThemeMode.dark);
+                          },
+      child: ClipOval(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+          child: Container(
+            height: 80,
+            width: 80,
+            decoration: BoxDecoration(
+              color: Color(0xff30393c).withOpacity(0.5), // 修正例
+              shape: BoxShape.circle,
+            ),
+             child: SvgPicture.asset(
+                                        AppVectors.moon,
+                                        fit: BoxFit.none,
+                                      ),
+          ), // Container
+        ), // BackdropFilter
+      ),
+    ),
+     const SizedBox(height: 15,),
+                        const Text(
+                          'Dark Mode',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 17,
+                            color: AppColors.grey
+                          ),
+                        )
+  ],
+),
+
+    SizedBox(width: 40,),
+  Column(
+    children: [
+       GestureDetector(
+                          onTap: (){
+                            context.read<ThemeCubit>().updateTheme(ThemeMode.light);
+                          },
+        child: ClipOval(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+          child: Container(
+            height: 80,
+            width: 80,
+            decoration: BoxDecoration(
+              color: Color(0xff30393c).withOpacity(0.5), // 修正例
+              shape: BoxShape.circle,
+            ),
+             child: SvgPicture.asset(
+                                        AppVectors.sun,
+                                        fit: BoxFit.none,
+                                      ),
+          ), // Container
+        ), // BackdropFilter
+        ),
+      ),
+     const SizedBox(height: 15,),
+                        const Text(
+                          'Dark Mode',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 17,
+                            color: AppColors.grey
+                          ),
+                        )
+  ],
+),
+    ],
+  ),
+
+  const SizedBox(height: 40),
   BasicAppButton(
     onPressed: () {
       Navigator.push(
     context,
     MaterialPageRoute(
-      builder: (BuildContext context) => const ChooseModePage(),
+      builder: (BuildContext context) => const SignupOrSigninPage(),
     ),
   );
     },
